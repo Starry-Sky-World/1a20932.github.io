e1dc8c3bc27bcb1c70a4b66c36cb888f3ff6b8eb
liuqinyu <116363551+Starry-Sky-World@users.noreply.github.com>
2024-07-23 22:33:22 +0800

    Update

---
+++ assets/index.md.CyqNco3m.js
@@ -0,0 +1,18 @@
+import{V as n}from"./chunks/theme.AanP5PIP.js";import{c as r,I as o,k as a,j as t,o as i}from"./chunks/framework.B2Sicsqr.js";const s=t("p",null,[t("br"),t("br"),t("br"),t("br"),t("br"),t("br")],-1),l=t("h1",{style:{"text-align":"center"}},"作者/贡献者",-1);console.log(` * ░░░░░░░░░░░░░░░░░░░░░░░░▄░░
+ * ░░░░░░░░░▐█░░░░░░░░░░░▄▀▒▌░
+ * ░░░░░░░░▐▀▒█░░░░░░░░▄▀▒▒▒▐
+ * ░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+ * ░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+ * ░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+ * ░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒
+ * ░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+ * ░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄
+ * ░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒
+ * ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒
+ * 修勾就这样默默地看着你~
+/////////////////////////////////////////////////////////
+//                                                     //
+//            版权所有: StarrySkyWorld                  //
+//            官方网站: https://superlinkstudio.top     //
+//                                                     //
+/////////////////////////////////////////////////////////`);const k=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"StarryFun","text":"星趣","image":{"alt":"logo","src":"logo.png"},"actions":[{"theme":"brand","text":"快速开始","link":"/project/index.md"},{"theme":"alt","text":"公告","link":"/notice/index.md"}]},"features":[{"title":"SuperLinkStudio","icon":"👨‍💻","details":"我所在的工作室","link":"https://superlinkstudio.top/"},{"title":"BPB-Worker-Panel-Chinese","icon":"🔓","details":"基于Cloudflare Worker的VPN节点搭建器 绕过GFW,从我做起！","link":"https://github.com/Starry-Sky-World/BPB-Worker-Panel-Chinese"},{"title":"SMSBoom-HXCZ","icon":"📱","details":"Python短信轰炸器，目前暂停维护","link":"https://github.com/Starry-Sky-World/SMSboom-HXCZ"}]},"headers":[],"relativePath":"index.md","filePath":"index.md"}'),d={name:"index.md"},S=Object.assign(d,{setup(c){const e=[{avatar:"https://github.com/Starry-Sky-World.png",name:"StarrySkyWorld",title:"站长",links:[{icon:"github",link:"https://github.com/Starry-Sky-World"}]}];return(m,h)=>(i(),r("div",null,[s,l,o(a(n),{size:"small",members:e})]))}});export{k as __pageData,S as default};
